66af104d500b6e37d825e0f04f2bcb86c41316a5
Jeffrey LeBlanc <jeff29922@outlook.com>
2025-05-25 19:10:17 -0400

    Add Pester tests and instructions

---
+++ tests/ProvisionTeamsPhoneUsers.Tests.ps1
@@ -0,0 +1,23 @@
+$scriptPath = Join-Path $PSScriptRoot '..' 'ProvisionTeamsPhoneUsers.ps1'
+$usersCsv   = Join-Path $PSScriptRoot 'data' 'Users.csv'
+$didsCsv    = Join-Path $PSScriptRoot 'data' 'dids.csv'
+
+Describe 'ProvisionTeamsPhoneUsers script' {
+    BeforeAll {
+        function Get-Credential {
+            param([string]$Message)
+            $secure = ConvertTo-SecureString 'pass' -AsPlainText -Force
+            New-Object System.Management.Automation.PSCredential('user',$secure)
+        }
+        function Import-Module {
+            param([string]$Name)
+        }
+        function Connect-MicrosoftTeams {}
+        function Grant-CsOnlineVoiceRoutingPolicy {}
+        function Set-CsPhoneNumberAssignment {}
+        function Add-Content {}
+    }
+    It 'runs in WhatIf mode without errors' {
+        { & $scriptPath -UserCsv $usersCsv -DidCsv $didsCsv -ThrottleLimit 1 -WhatIf } | Should -Not -Throw
+    }
+}
